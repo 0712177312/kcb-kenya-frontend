05f924f094218771e74cc0f248bb9c4ed4e901c5
Peter Mungai <peter.chege@compulynx.com>
2019-05-14 14:00:23 +0300

    Changed the database being accessed and the urls

--- client/src/app/compas/services/url.ts
+++ client/src/app/compas/services/url.ts
@@ -1,6 +1,7 @@
 export class Urls {
-    private uri = 'rest/v1';
-   // private uriTest = 'http://localhost:8090/compas/rest/v1';
+    //private uri = 'rest/v1';
+    //private uri = 'http://localhost:8090/compas/rest/v1';
+    private uri = 'http://172.16.21.72:8090/compas/rest/v1'
 
     get url(): string {
         return this.uri; // = 'rest/v1' ;
