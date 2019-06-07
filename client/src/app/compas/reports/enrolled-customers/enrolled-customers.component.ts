1de448067d6e6d0e7a276a223d18d5f667a439ef
Peter Mungai <peter.chege@compulynx.com>
2019-06-07 12:43:25 +0300

    Added ability to check logs for those rejected and deleted

--- client/src/app/compas/reports/enrolled-customers/enrolled-customers.component.ts
+++ client/src/app/compas/reports/enrolled-customers/enrolled-customers.component.ts
@@ -37,7 +37,7 @@ export class EnrolledCustomersComponent implements OnInit {
 
   ngOnInit() {
     console.log('init');
-    this.customerStatus = [{ name: 'Enrolled', id: 'N' }, { name: 'Verified', id: 'A' }];
+    this.customerStatus = [{ name: 'Enrolled', id: 'N' }, { name: 'Verified', id: 'A' }, { name: 'Rejected', id: 'R' }, { name: 'Deleted', id: 'D' }];
   }
   get today() {
     return new Date();
