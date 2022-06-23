1aa9233deb568ad6598e174535cea50ebd4d0d15
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2022-06-23 16:37:01 +0300

    url fix

--- src/app/compas/services/url.ts
+++ src/app/compas/services/url.ts
@@ -1,7 +1,7 @@
 export class Urls {
-    // private uri = 'rest/v1'; // to be used on server
-    //  private uri = 'http://localhost:9000/compas/rest/v1'; // to be used for local testing
-    private uri = 'https://8e8a-197-220-114-46.in.ngrok.io/compas/rest/v1'
+    private uri = 'rest/v1'; // to be used on server
+    //private uri = 'http://localhost:8080/compas/rest/v1'; // to be used for local testing
+    // private uri = 'https://8e8a-197-220-114-46.in.ngrok.io/compas/rest/v1'
 
 
 
