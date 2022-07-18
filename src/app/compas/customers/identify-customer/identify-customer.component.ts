51f47894a566c94705e25052ead8f7e3dd0525ad
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2022-07-18 14:48:05 +0300

    fixed bugs and removed stop device button for greenbit devices

--- src/app/compas/customers/identify-customer/identify-customer.component.ts
+++ src/app/compas/customers/identify-customer/identify-customer.component.ts
@@ -492,7 +492,7 @@ export class IdentifyCustomerComponent implements OnInit, OnDestroy {
         this.btnsCaptured = [];
         this.btnClass = false;
         this.disconnect();
-        // this.router.navigate(['./']);
+        this.router.navigate(['./']);
     }
 
     resetDevice() {
